c69c9a4c0ce7aa2e726cf0ae513c9a955e1d3bf4
CHANDESHWAR26 <chandeshwarcloud@gmail.com>
2025-04-09 19:43:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ let display = document.getElementById("display");
 function appendValue(value) {
   display.value += value;
 }
-
 function clearDisplay() {
   display.value = "";
 }
-
-
 function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
-
-
 function calculate() {
   try {
     display.value = eval(display.value);
@@ -20,10 +15,8 @@ function calculate() {
     display.value = "Error";
   }
 }
-
 document.addEventListener("keydown", function (e) {
   const key = e.key;
-
   if (!isNaN(key) || ['+', '-', '*', '/', '.', '%'].includes(key)) {
     appendValue(key);
   } else if (key === 'Enter') {
